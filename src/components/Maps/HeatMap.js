0ac428095858032e8f56678f497e077a05c89acd
Dan Phillips <dlphillips@gmail.com>
2020-03-29 16:40:23 -0400

    fix heat map

--- src/components/Maps/HeatMap.js
+++ src/components/Maps/HeatMap.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Map, TileLayer } from 'react-leaflet'
 import HeatmapLayer from 'react-leaflet-heatmap-layer'
-import { addressPoints } from './realworld.10000'
-
+import data from '../../data/data.json'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const tfKey = process.env.REACT_APP_THUNDERFOREST_KEY
+const points = data.features
 
 const HeatMap = props => {
   const { baseMap, onMapMove, lat, lng } = props
@@ -16,10 +16,10 @@ const HeatMap = props => {
     <div>
       <Map center={position} zoom={Number(props.zoom)} onMoveend={onMapMove}>
         <HeatmapLayer
-          points={addressPoints}
-          longitudeExtractor={m => m[1]}
-          latitudeExtractor={m => m[0]}
-          intensityExtractor={m => parseFloat(m[2])}
+          points={points}
+          longitudeExtractor={m => m.properties.Long_}
+          latitudeExtractor={m => m.properties.Lat}
+          intensityExtractor={m => m.properties.Confirmed}
         />
         <TileLayer
           attribution={baseMap.attribution}
