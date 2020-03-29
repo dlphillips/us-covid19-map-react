26028946576f6b789c67e3beb0dc781fbfa4d7a7
Dan Phillips <dlphillips@gmail.com>
2020-03-29 14:57:05 -0400

    get geojson data on the map

--- src/components/Maps/CovidMap.js
+++ src/components/Maps/CovidMap.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import dotenv from 'dotenv'
+import data from '../../data/data.json'
 
 dotenv.config()
 const tfKey = process.env.REACT_APP_THUNDERFOREST_KEY
+// const minValue = 1
+// const minRadius = 2
 
 const CovidMap = props => {
   const { baseMap, onMapMove, lat, lng } = props
@@ -15,6 +18,8 @@ const CovidMap = props => {
         attribution={baseMap.attribution}
         url={baseMap.apiKey ? `${baseMap.url}?apikey=${tfKey}` : baseMap.url}
       />
+      <GeoJSON data={data} />
+      {/* <GeoJSON data={data} onEachFeature={this.onEachFeaturePoint.bind(this)} pointToLayer={this.pointToLayer.bind(this)} /> */}
     </Map>
   )
 }
