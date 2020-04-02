4c1f402cf59fb664c55ccd52938193e4f40f30b1
Dan Phillips <dlphillips@gmail.com>
2020-04-02 13:44:48 -0400

    titlebar, 04012020 update

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import { withStyles, makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu'
-import Switch from '@material-ui/core/Switch'
-import FormGroup from '@material-ui/core/FormGroup'
+import { Switch, Slider, Grid, Paper, Typography } from '@material-ui/core/'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-// import {Select, InputLabel, MenuItem } from '@material-ui/core/'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -23,8 +19,52 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('sm')]: {
       display: 'block'
     }
-  }
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: '#ff7900',
+    fontWeight: "bold",
+    backgroundColor: '#fff',
+    verticalAlign: "baseline",
+    marginBottom: "10px"
+  },
 }))
+
+const PrettoSlider = withStyles({
+  root: {
+    color: '#ff7900',
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: '#fff',
+    border: '2px solid currentColor',
+    marginTop: -8,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: 'calc(-50% + 4px)',
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+const valuetext = value => {
+  console.log('value: ', value);
+  return `${value}`;
+}
 
 export default function SearchAppBar (props) {
 
@@ -32,7 +72,7 @@ export default function SearchAppBar (props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
+      <Paper className={classes.paper}>
         <Toolbar>
           <IconButton
             edge='start'
@@ -41,30 +81,50 @@ export default function SearchAppBar (props) {
             aria-label='open drawer'
             onClick={props.toggleDrawer()}
           >
-            <MenuIcon />
+          <MenuIcon />
           </IconButton>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch
-                  color='secondary'
-                  checked={props.heatMap}
-                  onChange={props.handleToggleHeatMap}
-                />
-              }
-              label='Heat Map'
-            />
-          </FormGroup>
-          {/* <InputLabel id="label">Select Date</InputLabel>
-          <Select labelId="label" id="select" value="20">
-            <MenuItem value="10">Ten</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-          </Select> */}
-          <Typography className={classes.title} variant='h6' noWrap>
+          <h2>
             US COVID-19 Cases by County
-          </Typography>
+          </h2>
         </Toolbar>
-      </AppBar>
+      </Paper>
+      <Grid container spacing={1} alignItems="stretch">
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>
+            <PrettoSlider
+              valueLabelDisplay="auto"
+              aria-label="pretto slider"
+              defaultValue={10}
+              getAriaValueText={valuetext}
+              step={1}
+              min={1}
+              max={10}
+            />
+            <Typography gutterBottom>
+              Radius Size (in process)
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+        <Paper className={classes.paper}>
+          <FormControlLabel
+            control={
+              <Switch
+                color='#fff'
+                checked={props.heatMap}
+                onChange={props.handleToggleHeatMap}
+              />
+            }
+            label='Heat Map'
+          />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>
+            Date Select Placeholder
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   )
 }
